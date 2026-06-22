3ac83addcefc74383e9a3b701d91c31acedb8684
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-06-21 21:58:01 -0300

    Simplifying the starting menu to user, graph and pixel actions

--- defines.py
+++ defines.py
@@ -9,4 +9,5 @@ PIXELA_ENDPOINT = "https://pixe.la/v1/users/"
 PIXELA_GRAPH_ENDPOINT = PIXELA_ENDPOINT + PIXELA_USER + "/graphs/"
 PIXELA_PIXEL_ENDPOINT = PIXELA_GRAPH_ENDPOINT + PIXELA_GRAPHID
 
-RESPONSE_SUCCESS = '{"message":"Success.","isSuccess":true}'
+RESPONSE_SUCCESS = "Success."
+RESPONSE_RETRY = "Please retry this request. Your request for some APIs will be rejected 25% of the time because you are not a Pixela supporter. If you are interested in being a Pixela supporter, please see: https://github.com/a-know/Pixela/wiki/How-to-support-Pixela-by-Patreon-%EF%BC%8F-Use-Limited-Features"
--- main.py
+++ main.py
@@ -6,43 +6,36 @@ import pixela_users
 def main():
     if __name__ == "__main__":
         uses_list = [
-            "create_user",
-            "create_graph",
-            "modify_graph",
-            "create_pixel_today",
-            "addto_pixel_today",
+            "",
+            "User Actions",
+            "Graph Actions",
+            "Pixel Actions",
             "quit",
         ]
 
         print("Options:\n")
-        for use in uses_list:
+        for use in uses_list[1:]:
             print(f"{use} [{uses_list.index(use)}]")
-        current_use_index = int(input("\t"))
-        current_use = uses_list[current_use_index]
+        current_use = int(input("\t"))
 
         # Generate Pixela user #
-        if current_use == uses_list[0]:
+        if current_use == 1:
             pixela_users.chose_user_acttion()
             # https://pixe.la/@lhgs
         #########################
 
         # Generate a graph
-        elif current_use == uses_list[1]:
-            pixela_graphs.create_graph()
+        elif current_use == 2:
+            pixela_graphs.chose_graph_action()
             # https://pixe.la/v1/users/lhgs/graphs/d0loc0graph.html
         #########################
 
-        # Update Graph
-        elif current_use == uses_list[2]:
-            pixela_graphs.modify_graph()
-        #########################
-
         # Generate a day pixel
-        elif current_use == uses_list[3] or current_use == uses_list[4]:
+        elif current_use == 3:
             pixela_pixels.chose_pixel_action()
         #########################
 
-        elif current_use == uses_list[5]:
+        elif current_use == 4:
             return
         else:
             return
